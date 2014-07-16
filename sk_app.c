d70412930612d890861a7d6f58a176f5fd977199
root <root@yhs-X551CAP.(none)>
2014-07-16 13:17:42 +0900

    open(), realease()

---
+++ sk_app.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	int fd;
+	
+	fd = open("/dev/SK", O_RDWR);
+	printf("fd=%d\n", fd);
+	
+	if(fd<0){
+	perror("/dev/SK error");
+	exit(-1);
+	}
+	else
+	printf("SK has been detected...\n");
+
+	getchar();
+	close(fd);
+
+	return 0;
+}
